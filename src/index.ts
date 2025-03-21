495d15e0be56b6e2a32f38dbe2acc035ba46d899
ocheezyyy <seanodonnellse@gmail.com>
2025-03-21 12:59:25 -0400

    Removed old redis url code

--- src/index.ts
+++ src/index.ts
@@ -14,12 +14,6 @@ const server = http.createServer(app);
 
 const redisClient: RedisClientType = createClient({
   url: process.env.REDIS_URL,
-  // username: process.env.REDIS_USER,
-  // password: process.env.REDIS_PASS,
-  // socket: {
-  //   host: process.env.REDIS_HOST,
-  //   port: Number(process.env.REDIS_PORT),
-  // },
 });
 
 redisClient
